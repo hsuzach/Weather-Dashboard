35f7c4093cc81c39f381f553e45a1224c5d4cc61
hsuzach <hsuzach@gmail.com>
2022-02-28 15:49:34 -0800

    recall last searched

--- script.js
+++ script.js
@@ -6,11 +6,13 @@
 let inputCity = document.getElementById("inputCity")
 let searchBtn = document.getElementById("searchBtn")
 let searchHistory = document.getElementById("searchHistory")
+let storedCities
 let savedCities = []
-let storedCities = []
+
 
 function searchForCity(event){
   event.preventDefault()
+  
 
   if (inputCity.value === ''){
     alert("Please, enter a City into the search form.");
@@ -18,34 +20,30 @@ function searchForCity(event){
   }
   
   searchHistory.textContent = ''
-
   console.log(inputCity.value)
 
   savedCities.push(inputCity.value)
-
-  localStorage.setItem("Searched", JSON.stringify(savedCities))
+  inputCity.value = ""
   
+  localStorage.setItem("Searched", JSON.stringify(savedCities))
   storedCities = JSON.parse(localStorage.Searched);
 
   console.log(storedCities)
   
- 
-  for (i=0; i < savedCities.length; i++){
-    $("#searchHistory").append("<li>" + storedCities[i] + "</li>");
+  for (i=0; i < storedCities.length; i++){
+    $("#searchHistory").append("<btn id=" + storedCities[i] + ">" + storedCities[i] + "</btn> <br>");
   }
 
-  storedCities = []
-  
-  inputCity.value = ""
-  
-  
 }
 
 searchBtn.addEventListener("click",searchForCity);
 
-// function recallSearched(){
-//   for (i=0; i < savedCities.length; i++){
-//   $("#searchHistory").append("<li>" + savedCities[i] + "</li>");
-//   }
-// }
-// recallSearched();
+function recallSearched(){
+
+  let storedCities = JSON.parse(localStorage.Searched);
+  for (i=0; i < storedCities.length; i++){
+    $("#searchHistory").append("<btn id=" + storedCities[i] + ">" + storedCities[i] + "</btn> <br>");
+  }
+  
+}
+recallSearched();
