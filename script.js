3031bf3da22250cfd58fe38753a750b4fb0ed34b
hsuzach <hsuzach@gmail.com>
2022-02-28 15:18:29 -0800

    save city array to local storage

--- script.js
+++ script.js
@@ -6,12 +6,46 @@
 let inputCity = document.getElementById("inputCity")
 let searchBtn = document.getElementById("searchBtn")
 let searchHistory = document.getElementById("searchHistory")
+let savedCities = []
+let storedCities = []
 
-function searchForCity(){
-  console.log(inputCity.value)
+function searchForCity(event){
+  event.preventDefault()
+
+  if (inputCity.value === ''){
+    alert("Please, enter a City into the search form.");
+    return
+  }
   
+  searchHistory.textContent = ''
 
+  console.log(inputCity.value)
+
+  savedCities.push(inputCity.value)
+
+  localStorage.setItem("Searched", JSON.stringify(savedCities))
+  
+  storedCities = JSON.parse(localStorage.Searched);
+
+  console.log(storedCities)
+  
+ 
+  for (i=0; i < savedCities.length; i++){
+    $("#searchHistory").append("<li>" + storedCities[i] + "</li>");
+  }
+
+  storedCities = []
+  
+  inputCity.value = ""
+  
+  
 }
 
 searchBtn.addEventListener("click",searchForCity);
 
+// function recallSearched(){
+//   for (i=0; i < savedCities.length; i++){
+//   $("#searchHistory").append("<li>" + savedCities[i] + "</li>");
+//   }
+// }
+// recallSearched();
